4563d5d78875bb308f19312fd2a63a536e8f5059
Rob Galanakis <rob.galanakis@gmail.com>
2024-01-23 08:21:36 -0800

    Add custom content to minisite

---
+++ _plugins/markdown_block.rb
@@ -0,0 +1,13 @@
+module Bikebuspdx
+  class MarkdownBlock < Liquid::Block
+    def initialize(tag_name, text, tokens)
+      super
+    end
+    require "kramdown"
+    def render(context)
+      content = super
+      "#{Kramdown::Document.new(content).to_html}"
+    end
+  end
+end
+Liquid::Template.register_tag('markdown', Bikebuspdx::MarkdownBlock)
