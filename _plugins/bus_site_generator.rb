a1019a09b89400b6a63042ad55cc7c44e159344e
Rob Galanakis <rob.galanakis@gmail.com>
2024-01-25 09:45:17 -0800

    Replace James John route image
    Make route image able to be explicit

--- _plugins/bus_site_generator.rb
+++ _plugins/bus_site_generator.rb
@@ -3,14 +3,17 @@ module Bikebuspdx
     def generate(site)
       dir = '_pages/buses'
       site.data['buses'].each do |bus|
-        site.pages << Jekyll::PageWithoutAFile.new(site, site.source, dir, bus['slug'] + ".html").tap do |file|
+        slug = bus['slug']
+        site.pages << Jekyll::PageWithoutAFile.new(site, site.source, dir, slug + ".html").tap do |file|
           file.data.merge!(
             "layout" => 'page',
             'title' => "#{bus['name']} Bike Bus",
             'navigation_exclude' => true,
-            'permalink' => "/#{bus['slug']}"
+            'permalink' => "/#{slug}",
+            'map_image' => "/assets/images/routes/route-#{slug}.png"
           )
           file.data['image'] = bus['image'] if bus['image']
+          file.data['map_image'] = bus['map_image'] if bus['map_image']
           kvps = bus.map { |k, v| "#{k}='#{v}' " }.join(' ')
           file.content = "{% include bus-minisite-content.html #{kvps} %}"
           file.output
