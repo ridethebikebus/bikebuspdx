7953f84bccedc313366741476463aa132914f16f
Rob Galanakis <rob.galanakis@gmail.com>
2024-04-22 16:50:32 -0700

    Add jamesjohn flyers, auto-email, multi-image
    - Use `<slug>@bikebuspdx.org` as a default email, so all bike buses
      have some contact info.
    - Support multiple map/route images.
      Needed for jamesjohn bilingual flyer.

--- _plugins/bus_site_generator.rb
+++ _plugins/bus_site_generator.rb
@@ -3,17 +3,20 @@ module Bikebuspdx
     def generate(site)
       dir = '_pages/buses'
       site.data['buses'].each do |bus|
-        slug = bus['slug']
+        slug = bus.fetch('slug')
+        name = bus.fetch('name')
         site.pages << Jekyll::PageWithoutAFile.new(site, site.source, dir, slug + ".html").tap do |file|
           file.data.merge!(
             "layout" => 'page',
-            'title' => "#{bus['name']} Bike Bus",
+            'title' => "#{name} Bike Bus",
             'navigation_exclude' => true,
             'permalink' => "/#{slug}",
           )
           file.data['image'] = bus['image'] if bus['image']
           include_data = bus.dup
-          include_data['map_image'] = bus['map_image'] ? bus['map_image'] : "/assets/images/routes/route-#{slug}.png"
+          include_data['email'] = bus['email'] || "#{slug}@bikebuspdx.org"
+          include_data['map_image'] = bus['map_image'] || "/assets/images/routes/route-#{slug}.png"
+          include_data['map_alt'] = bus['map_alt'] || "#{name} Bike Bus Route Map"
           kvps = include_data.map { |k, v| "#{k}='#{v}' " }.join(' ')
           file.content = "{% include bus-minisite-content.html #{kvps} %}"
           file.output
